b10dda29a7fb7af08e16fa1209d8dc2cb221c397
Remi Morisawa <morisawaremi@gmail.com>
2025-05-15 10:13:05 +0900

    Add sub, multi, and div

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
 let calc;
+
+function getCalculation(){
+
+};
+
 const add = function(...numToCalc) {
     const numbers = [];
     numbers.push(...numToCalc);
@@ -6,3 +11,25 @@ const add = function(...numToCalc) {
     calc = numToCalc.reduce(addNums);
 	return calc;
 };
+
+const subtract = function(...numToCalc) {
+    const numbers = [];
+    numbers.push(...numToCalc);
+    const subNums = (a, b)=> a - b;
+    calc = numToCalc.reduce(subNums);
+	return calc;
+};
+
+const multiply = function(...numToCalc) {
+    const numbers = [];
+    const multiplyNums = (a, b)=> a * b;
+    calc = numToCalc.reduce(multiplyNums);
+    return calc;
+  };
+
+  const divide = function(...numToCalc) {
+    const numbers = [];
+    const divideNums = (a, b)=> a / b;
+    calc = numToCalc.reduce(divideNums);
+    return calc;
+  };
